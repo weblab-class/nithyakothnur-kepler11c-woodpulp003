84d698592296c080f79938a3480f61daa2276da2
Nithya Kothnur <nithyakothnur@dhcp-10-29-148-188.dyn.MIT.EDU>
2024-01-19 23:19:13 -0500

    home layout added with happy cows

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import "../pages/Home.css";
 
@@ -6,7 +7,82 @@ const Home = (props) => {
   return (
     <body>
       <div className="title">Welcome to Sound Mixer!</div>
-      <div className="u-textCenter"> Add info text here</div>
+      <div className="main u-textCenter">
+        <div className="block">
+          <img src="./images/happycow.jpeg" />
+          <div>
+            <Link to="/learn" className="NavBar-link main-title">
+              Learn
+            </Link>
+          </div>
+          <p>
+            Navigate the wonders behind the science of waves and explore how use the features on
+            this website. "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+            deserunt mollit anim id est laborum."{" "}
+          </p>
+          <p>
+            Mix together different types of waveforms to create your own sounds. "Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <div className="block">
+          <img src="./images/happycow.jpeg" />
+          <div>
+            <Link to="/blend" className="NavBar-link main-title">
+              Blend
+            </Link>
+          </div>{" "}
+          <p>
+            Mix together different types of waveforms to create your own sounds. "Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>{" "}
+          <p>
+            Mix together different types of waveforms to create your own sounds. "Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <div className="block">
+          <img src="./images/happycow.jpeg" />
+          <div>
+            <Link to="/play" className="NavBar-link main-title">
+              Play
+            </Link>
+          </div>{" "}
+          <p>
+            Enjoy pre-set waveforms or your own saved sounds to play music on the piano. "Lorem
+            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+            est laborum."
+          </p>{" "}
+          <p>
+            Mix together different types of waveforms to create your own sounds. "Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+      </div>
     </body>
   );
 };
