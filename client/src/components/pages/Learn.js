c04a0f5d04ba88aad0c72f9d4ef45847aca5fa03
Nithya Kothnur <nithya.kothnur@gmail.com>
2024-01-31 11:45:55 -0500

    made learn pretty

--- client/src/components/pages/Learn.js
+++ client/src/components/pages/Learn.js
@@ -167,7 +167,7 @@ const Learn = () => {
   return (
     <>
       <div class="flex flex-row">
-        <aside>
+        <aside class="sticky top-0 self-start">
           <div class="px-5">
             <ul id="sidebar" class="flex-1">
               <li id="head_mark">
@@ -205,99 +205,146 @@ const Learn = () => {
             </ul>
           </div>
         </aside>
-        <div class="flex-1">
-          <div id="sound_as_waves">
-            <h1 class="text-2xl">Sound waves</h1>
+        <div class="flex-1 px-4">
+          <div id="sound_as_waves" class="whitespace-pre-line text-center">
+            <h1 class="text-2xl">What are Soundwaves?</h1>
             <hr></hr>
             <p>
               Have you ever wondered what sound really is? How is it that we can hear the difference
               between a piano and a guitar? And why do some notes sound shriller than others? Why do
               some notes sound good together, and other combinations make your ears bleed?
             </p>
+            <p class="m-4"></p>
             <p>
               The answers to all these questions can be understood once you understand sound as
               waves, and model their characteristics in frequency and amplitude spaces.
               Fundamentally, each sound is a vibration, just air moving really fast periodically.
             </p>
+            <p class="m-4"></p>
+
             <p>
               Let's first understand how the time period, amplitude and shape of the wave dictates
               how it sounds to us.
             </p>
-            <h3>Time period</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Time period</h3>
             <p>
               Here's the deal: the faster the air vibrates, the shriller it sounds. Increasing the
               frequency or decreasing the time period makes the waves sound shriller!
             </p>
-            <div class="flex flex-row gap-4">
+            <p class="m-4"></p>
+
+            <div class="flex flex-row gap-4 justify-center">
               <button
                 id="play440Hz"
-                class="hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class=" p-1 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={playfreq}
               >
                 Play 440Hz
               </button>
               <button
                 id="kill440Hz"
-                class="hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class=" p-1 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={killfreq}
               >
                 Stop 440Hz
               </button>
               <button
                 id="play880Hz"
-                class="hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class=" p-1 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={playfreq}
               >
                 Play 880Hz
               </button>
               <button
                 id="kill880Hz"
-                class="hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                class=" p-1 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={killfreq}
               >
                 Stop 880Hz
               </button>
             </div>
-            <h3>Amplitude</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Amplitude</h3>
             <p>
               The amplitude of the wave represents how much the pressure varies in each oscillation.
               The loudness of the sound is directly related to the amplitude of the pressure waves.
             </p>
-            <h3>Wave Shape</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Wave Shape</h3>
             <p>
               The wave shape affects the quality of the sound: it is what differentiates a piano
               from a guitar, or your voice from mine! There are four basic wave shapes that are
               commonly used in sound synthesis: sine, square, triangle and sawtooth. Let's hear how
               each of these sound!
             </p>
-            <button id="playsine" onClick={playfreq}>
+            <p class="m-4"></p>
+
+            <button
+              id="playsine"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={playfreq}
+            >
               Play Sine
             </button>
-            <button id="killsine" onClick={killfreq}>
+            <button
+              id="killsine"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={killfreq}
+            >
               Stop Sine
             </button>
-            <button id="playsquare" onClick={playfreq}>
+            <button
+              id="playsquare"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={playfreq}
+            >
               Play Square
             </button>
-            <button id="killsquare" onClick={killfreq}>
+            <button
+              id="killsquare"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={killfreq}
+            >
               Stop Square
             </button>
             {/* <Sine /> */}
             {/* <Fourier /> */}
             {/* const ctx = document.getElementById("canvas").getContext("2d"); plotSine(cts); */}
             {/* <SineGraph /> */}
-            <button id="playtriangle" onClick={playfreq}>
+            <button
+              id="playtriangle"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={playfreq}
+            >
               Play Triangle
             </button>
-            <button id="killtriangle" onClick={killfreq}>
+            <button
+              id="killtriangle"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={killfreq}
+            >
               Stop Triangle
             </button>
-            <button id="playsawtooth" onClick={playfreq}>
+            <button
+              id="playsawtooth"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={playfreq}
+            >
               Play Sawtooth
             </button>
-            <button id="killsawtooth" onClick={killfreq}>
+            <button
+              id="killsawtooth"
+              class=" p-1 ml-2 mr-2 border-2 hover:text-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={killfreq}
+            >
               Stop Sawtooth
             </button>
+            <p class="m-4"></p>
+
             <p>
               There is a lot that can be said about these wave shapes, but what one needs to
               understand is that ALL waves are a sum of a bunch of sine waves. This is the key idea
@@ -313,6 +360,8 @@ const Learn = () => {
               the frequency of the fundamental and so on. The existence of these multiples of the
               fundamental will be crucial later.
             </p>
+            <p class="m-4"></p>
+
             {/* Include buttons for the four sound types */}
 
             {/* const ctx = document.getElementById("canvas").getContext("2d"); plotSine(cts); */}
@@ -323,45 +372,52 @@ const Learn = () => {
               powerful ways to make sounds dynamic is to introduce an ADSR envelope, more on that in
               the next section.
             </p>
+            <p class="m-4"></p>
           </div>
-          <div id="what_is_an_envelope">
+          <div id="what_is_an_envelope" class="whitespace-pre-line text-center">
             <h1 class="text-2xl">What is an Envelope?</h1>
             <hr></hr>
             <p>In this section, we will understand what an envelope is, and what ADSR means.</p>
-            <h3>Changing amplitude over time and the envelope</h3>
+            <p class="m-4"></p>
+            <h3 class="text-xl">Changing amplitude and the enveleope over time</h3>
             <p>
               Most implementations of an electronic synth change the gain, or volume, of the sound
               over time to give the instrument some motion. The motion can be graphed with respect
               to time to give an envelope of the volume. This envelope is described by the four
               parameters Attack, Decay, Sustain and Release; or ADSR.
             </p>
-            <h3>Attack</h3>
+            <p class="m-4"></p>
+            <h3 class="text-xl">Attack</h3>
             The attack is the time that the sound takes to reach its maximum amplitude from the
             moment of triggering. A smaller attack sounds abrupt, while a longer attack makes the
             sound swell up for a longer time.
-            <h3>Decay</h3>
+            <h3 class="text-xl">Decay</h3>
+            <p class="m-4"></p>
             <p>
               Decay is the time that the sound takes after reaching the maximum amplitude to drop
               down to its sustained volume: the volume that plays while you still hold down the key.
               A short decay time means the sound immediately drops in volume after the attack time
               is over.
             </p>
-            <h3>Sustain</h3>
+            <p class="m-4"></p>
+            <h3 class="text-xl">Sustain</h3>
             <p>
               This is the ratio of the maximum volume reached by the sound and that which is heard
               while you hold down the key. Its a value between 0 and 1; a value closer to 1 means
               the shift between the attack sound and the sustained sound is very smooth.
             </p>
-            <h3>Release</h3>
+            <p class="m-4"></p>
+            <h3 class="text-xl">Release</h3>
             <p>
               This is the time for which the sound fades out after you release the key. A long decay
               time means the sound will be heard for longer, provided you held the note long enough
               to clear the attack time. Imagine playing a lot of these notes at the same time, they
               keep playing together, decaying after the decay time!
             </p>
+            <p class="m-4"></p>
             <p>
-              <p>
-                Now try your hand at making ADSR changes and watch the graph evolve.
+              <p>Now try your hand at making ADSR changes and watch the graph evolve.</p>
+              <p class="w-1/4 items-center">
                 <EnvelopeGraph a={attack} d={decay} s={sustain} r={release} />
                 <div>
                   <h3>attack</h3>
@@ -374,9 +430,10 @@ const Learn = () => {
                   <input type="range" step="0.1" onChange={changeRelease} min="0" max="1" />
                 </div>
               </p>
+              <p class="m-4"></p>
             </p>
           </div>
-          <div id="filter_controls">
+          <div id="filter_controls" class="text-center">
             {/* <h1 class="text-2xl">Filter Controls</h1>
             <hr></hr>
             <p>
@@ -406,20 +463,26 @@ const Learn = () => {
               filter is defined by the frequency-cutoff, the quality Q and filter type amongst other
               parameters.
             </p>
-            <h3>Frequency-Cutoff</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Frequency-Cutoff</h3>
             <p>
               This defines the lower (or upper) limit of the frequency beyond which you cannot hear
               the sound. For example, for a low-pass filter, if the frequency-cutoff is at 400Hz, no
               sounds below a frequency of 400Hz will be audible.
             </p>
-            <h3>Quality, Q</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Quality, Q</h3>
             <p>
               This defines how sharply the filter applies and how specific the filter behavior is to
               the value of the frequency-cutoff. For example, in a notch type filter, a higher value
               of Q means that only a very specific range of frequencies around the frequency-cutoff
               are silenced.
             </p>
-            <h3>Filter Type</h3>
+            <p class="m-4"></p>
+
+            <h3 class="text-xl">Filter Type</h3>
             <p>
               This produces the starkest difference amongst all other parameters as it completely
               defines how we bump up or silence the frequencies. A low-pass filter silences the
@@ -427,8 +490,9 @@ const Learn = () => {
               out in Blend to see for yourself how a square wave sounds more and more like a sine
               wave when you decrease the frequency-cutoff for a low-pass filter.
             </p>
+            <p class="m-4"></p>
           </div>
-          <div id="music_of_it_all">
+          <div id="music_of_it_all" class="text-center">
             <h1 class="text-2xl">Music of it All:</h1>
             <hr></hr>
             <p>
@@ -440,6 +504,8 @@ const Learn = () => {
               that are seperated by a ratio of 2, while a perfect fifth (like C to G on the piano)
               represents a ratio of 3:2.
             </p>
+            <p class="m-4"></p>
+
             <p>
               The overall picture of sound design is thus this. We start with a note with some
               fundamental frequency. We add up the fundamental sine wave with this fundamental
@@ -454,12 +520,15 @@ const Learn = () => {
               note. Finally, we have a sound to ourselves, and we can combine together multiple
               versions of the fundamental frequency to create music.
             </p>
+            <p class="m-4"></p>
+
             <p>
               This website is a mere introduction to the world of sound design and synthesis. There
               are many more effects that we have forgone; you can add reverb by echoing the original
               sound, unison by detuning the fundamental. There is much more to learn, and hopefully
               we have excited you to continue on this journey!
             </p>
+            <p class="m-12"></p>
           </div>
         </div>
       </div>
